577695c4d7cdc5e8f4a6907e9be421ec9a7e6e85
Gabriela Rebeca <110422932+gabrielarebeca@users.noreply.github.com>
2023-10-19 20:04:33 -0300

    Alteração de arquivos

---
+++ Form1.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace WFACalculadora
+{
+    partial class FrmCalculadora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.lblNum2 = new System.Windows.Forms.Label();
+            this.lblNum1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.rdbMutiplicar = new System.Windows.Forms.RadioButton();
+            this.rdbSomar = new System.Windows.Forms.RadioButton();
+            this.rdbSubtrair = new System.Windows.Forms.RadioButton();
+            this.rdbDividir = new System.Windows.Forms.RadioButton();
+            this.txtNum2 = new System.Windows.Forms.TextBox();
+            this.txtNum1 = new System.Windows.Forms.TextBox();
+            this.lblOper = new System.Windows.Forms.Label();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(272, 128);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(88, 32);
+            this.btnLimpar.TabIndex = 0;
+            this.btnLimpar.Text = "&Limpar";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // lblNum2
+            // 
+            this.lblNum2.AutoSize = true;
+            this.lblNum2.Location = new System.Drawing.Point(281, 34);
+            this.lblNum2.Name = "lblNum2";
+            this.lblNum2.Size = new System.Drawing.Size(57, 13);
+            this.lblNum2.TabIndex = 4;
+            this.lblNum2.Text = "2° Número";
+            // 
+            // lblNum1
+            // 
+            this.lblNum1.AutoSize = true;
+            this.lblNum1.Location = new System.Drawing.Point(88, 34);
+            this.lblNum1.Name = "lblNum1";
+            this.lblNum1.Size = new System.Drawing.Size(57, 13);
+            this.lblNum1.TabIndex = 5;
+            this.lblNum1.Text = "1° Número";
+            this.lblNum1.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.rdbMutiplicar);
+            this.groupBox1.Controls.Add(this.rdbSomar);
+            this.groupBox1.Controls.Add(this.rdbSubtrair);
+            this.groupBox1.Controls.Add(this.rdbDividir);
+            this.groupBox1.Location = new System.Drawing.Point(64, 90);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(102, 116);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Operações";
+            // 
+            // rdbMutiplicar
+            // 
+            this.rdbMutiplicar.AutoSize = true;
+            this.rdbMutiplicar.Location = new System.Drawing.Point(6, 88);
+            this.rdbMutiplicar.Name = "rdbMutiplicar";
+            this.rdbMutiplicar.Size = new System.Drawing.Size(72, 17);
+            this.rdbMutiplicar.TabIndex = 13;
+            this.rdbMutiplicar.Text = "&Multiplicar";
+            this.rdbMutiplicar.UseVisualStyleBackColor = true;
+            this.rdbMutiplicar.CheckedChanged += new System.EventHandler(this.rdbMutiplicar_CheckedChanged);
+            // 
+            // rdbSomar
+            // 
+            this.rdbSomar.AutoSize = true;
+            this.rdbSomar.Checked = true;
+            this.rdbSomar.Location = new System.Drawing.Point(6, 19);
+            this.rdbSomar.Name = "rdbSomar";
+            this.rdbSomar.Size = new System.Drawing.Size(55, 17);
+            this.rdbSomar.TabIndex = 10;
+            this.rdbSomar.TabStop = true;
+            this.rdbSomar.Text = "&Somar";
+            this.rdbSomar.UseVisualStyleBackColor = true;
+            this.rdbSomar.CheckedChanged += new System.EventHandler(this.rdbSomar_CheckedChanged);
+            // 
+            // rdbSubtrair
+            // 
+            this.rdbSubtrair.AutoSize = true;
+            this.rdbSubtrair.Location = new System.Drawing.Point(6, 42);
+            this.rdbSubtrair.Name = "rdbSubtrair";
+            this.rdbSubtrair.Size = new System.Drawing.Size(61, 17);
+            this.rdbSubtrair.TabIndex = 11;
+            this.rdbSubtrair.Text = "Su&btrair";
+            this.rdbSubtrair.UseVisualStyleBackColor = true;
+            this.rdbSubtrair.CheckedChanged += new System.EventHandler(this.rdbSubtrair_CheckedChanged);
+            // 
+            // rdbDividir
+            // 
+            this.rdbDividir.AutoSize = true;
+            this.rdbDividir.Location = new System.Drawing.Point(6, 65);
+            this.rdbDividir.Name = "rdbDividir";
+            this.rdbDividir.Size = new System.Drawing.Size(54, 17);
+            this.rdbDividir.TabIndex = 12;
+            this.rdbDividir.Text = "&Dividir";
+            this.rdbDividir.UseVisualStyleBackColor = true;
+            this.rdbDividir.CheckedChanged += new System.EventHandler(this.rdbDividir_CheckedChanged);
+            // 
+            // txtNum2
+            // 
+            this.txtNum2.Location = new System.Drawing.Point(237, 50);
+            this.txtNum2.Name = "txtNum2";
+            this.txtNum2.Size = new System.Drawing.Size(147, 20);
+            this.txtNum2.TabIndex = 0;
+            // 
+            // txtNum1
+            // 
+            this.txtNum1.Location = new System.Drawing.Point(44, 50);
+            this.txtNum1.Name = "txtNum1";
+            this.txtNum1.Size = new System.Drawing.Size(148, 20);
+            this.txtNum1.TabIndex = 1;
+            // 
+            // lblOper
+            // 
+            this.lblOper.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOper.Location = new System.Drawing.Point(198, 50);
+            this.lblOper.Name = "lblOper";
+            this.lblOper.Size = new System.Drawing.Size(38, 20);
+            this.lblOper.TabIndex = 7;
+            this.lblOper.Text = "+";
+            this.lblOper.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblOper.Click += new System.EventHandler(this.lblOper_Click);
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(272, 90);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(88, 32);
+            this.btnCalcular.TabIndex = 9;
+            this.btnCalcular.Text = "&Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.Location = new System.Drawing.Point(272, 166);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(88, 32);
+            this.btnSair.TabIndex = 10;
+            this.btnSair.Text = "Sai&r";
+            this.btnSair.UseVisualStyleBackColor = true;
+            this.btnSair.Click += new System.EventHandler(this.btnSair_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(172, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 16);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Calculadora";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // FrmCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 220);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.lblOper);
+            this.Controls.Add(this.txtNum2);
+            this.Controls.Add(this.txtNum1);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.lblNum1);
+            this.Controls.Add(this.lblNum2);
+            this.Controls.Add(this.btnLimpar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "FrmCalculadora";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.FrmCalculadora_FormClosing);
+            this.Load += new System.EventHandler(this.FrmCalculadora_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLimpar;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label lblNum2;
+        private System.Windows.Forms.Label lblNum1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtNum2;
+        private System.Windows.Forms.TextBox txtNum1;
+        private System.Windows.Forms.RadioButton rdbMutiplicar;
+        private System.Windows.Forms.RadioButton rdbSomar;
+        private System.Windows.Forms.RadioButton rdbSubtrair;
+        private System.Windows.Forms.RadioButton rdbDividir;
+        private System.Windows.Forms.Label lblOper;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label label1;
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,107 @@
+﻿    using System;
+using System.Windows.Forms;
+
+namespace WFACalculadora
+{
+    public partial class FrmCalculadora : Form
+    {
+        public FrmCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FrmCalculadora_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void rdbSomar_CheckedChanged(object sender, EventArgs e)
+        {
+            lblOper.Text = "+";
+        }
+
+        private void rdbSubtrair_CheckedChanged(object sender, EventArgs e)
+        {
+            lblOper.Text = "-";
+        }
+
+        private void rdbDividir_CheckedChanged(object sender, EventArgs e)
+        {
+            lblOper.Text = "/";
+        }
+
+        private void rdbMutiplicar_CheckedChanged(object sender, EventArgs e)
+        {
+            lblOper.Text = "x";
+        }
+
+        private void FrmCalculadora_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Deseja mesmo sair?", "Atenção", MessageBoxButtons.YesNo,
+               MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.No)
+           {
+               e.Cancel = true;
+            }
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            if (rdbSomar.Checked)
+            {
+                MessageBox.Show("A soma é: " + (Convert.ToDouble(txtNum1.Text) + double.Parse(txtNum2.Text))
+                    , "Resposta:", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (rdbSubtrair.Checked)
+            {
+                MessageBox.Show("A subtração é: " + (Convert.ToDouble(txtNum1.Text) - double.Parse(txtNum2.Text))
+                    , "Resposta:", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (rdbDividir.Checked)
+            {
+                MessageBox.Show("A Divisão é: " + (Convert.ToDouble(txtNum1.Text) / double.Parse(txtNum2.Text))
+                    , "Resposta:", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (rdbMutiplicar.Checked)
+            {
+                MessageBox.Show("O produto é: " + (Convert.ToDouble(txtNum1.Text) * double.Parse(txtNum2.Text))
+                    , "Resposta:", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnSair_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Deseja mesmo sair?", "Atenção", MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question, MessageBoxDefaultButton.Button2) == DialogResult.Yes)
+            {
+                Close();
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            rdbDividir.Checked = false;
+            rdbMutiplicar.Checked = false;
+            rdbSomar.Checked = false;
+            rdbSubtrair.Cheked = false;
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblOper_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
